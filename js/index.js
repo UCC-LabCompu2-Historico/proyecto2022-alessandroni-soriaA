3570c2c50dcb9bf4ad03d639deb929c4ed533ee2
jysoria <2011814@edu.ucc.ar>
2022-06-13 15:46:40 -0300

    boton busqueda

--- js/index.js
+++ js/index.js
@@ -21,12 +21,10 @@ function dibujarImagen (x, y) {
 
 }
 
-
-x=0;
-dx=2; //como se va a ir moviendo
+var x=0;
+var dx=2; //como se va a ir moviendo
 
 function animarAvion () {
-
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
@@ -36,15 +34,22 @@ function animarAvion () {
     img.src = "imagenes/avion1.png";
 
     img.onload = function () {
-
         ctx.drawImage(img, x, 100);
-
     }
 
     if (x>canvas.width) {
-
         x=0;
     }
 
     x = x + dx;
+}
+
+function Destino() {
+    var pais = document.getElementById("Destino").value;
+
+    var url;
+
+    console.log(pais);
+    url = "Cancun.html";
+    window.open(url);
 }
