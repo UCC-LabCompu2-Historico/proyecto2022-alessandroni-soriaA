3bccff2bca09db57e400c21dd29c1e2492a83b0b
orne <2115318@ucc.edu.ar>
2022-06-14 02:11:24 -0300

    agrego imagenes y js

---
+++ js/destinos.js
@@ -0,0 +1,28 @@
+const fila = document.querySelector(".contenedor-carrusel");
+const destinos = document.querySelectorAll(".destinos");
+
+const flechaIzq = document.getElementById("flecha-izquierda");
+const flechaDer = document.getElementById("flecha-derecha");
+
+ flechaDer.addEventListener("click", () =>  {
+
+     fila.scrollLeft += fila.offsetWidth;
+
+ })
+
+flechaIzq.addEventListener("click", () =>  {
+
+    fila.scrollLeft -= fila.offsetWidth;
+
+})
+
+/*indicadores*/
+
+const numeroPaginas = Math.ceil(destinos.length / 3);
+ for (let i=0; i<numeroPaginas; i++) {
+
+     const indicador = document.createElement("button");
+     document.querySelector(".indicadores").appendChild(indicador);
+
+         }
+
