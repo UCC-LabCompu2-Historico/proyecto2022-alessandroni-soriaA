f9ba6bde98352174d02e97bd453a4b81f627dbf1
jysoria <2011814@edu.ucc.ar>
2022-06-14 10:32:21 -0300

    intentando que ande el test

--- js/test.js
+++ js/test.js
@@ -1,40 +1,74 @@
-const contenedor = document.getElementById("test");
+const miForm = document.getElementById("test");
 const botonResul = document.getElementById("botonResul");
 const resultadoTest = document.getElementById("resultado");
 
+var motivo = document.getElementById("motivo").value;
+var placer = document.getElementById("Placer").value;
+var negocios = document.getElementById("Negocios").value;
+
+var personas = document.getElementById("personas").value;
+var solo = document.getElementById("Solo").value;
+var familia = document.getElementById("Familia").value;
+var amigos = document.getElementById("Amigos").value;
+
+var precio = document.getElementById("precio").value;
+var economicos = document.getElementById("Economicos").value;
+var turisticos = document.getElementById("Turisticos").value;
+
+var arreglo = [motivo, personas, precio];
 
 function asignaValores() {
+    var total = 3;
+  
+    for (var i = 1; i < total; i++) {
+        
+        if (motivo === null || motivo === ""){
+            alert("Por favor responda la pregunta 1");
+            return false;
+        } else {
+            if(motivo === 1){
+                arreglo[motivo] = 1;
+            } else {
+                arreglo[motivo] = 2;
+            }
+        }
 
-    var motivo = document.getElementById("motivo");
-    var placer = document.getElementById("Placer");
-    var negocios = document.getElementById("Negocios");
+        if (personas === null || personas === ""){
+            alert("Por favor responda la pregunta 2");
+            return false;
+        } else {
+            if(personas === 3){
+                arreglo[personas] = 3;
+            } else {
+                if(personas === 4){
+                    arreglo[personas] = 4;
+                } else {
+                    arreglo[personas] = 5;
+                }
+            }
+        }
 
-    var personas = document.getElementById("personas");
-    var solo = document.getElementById("Solo");
-    var familia = document.getElementById("Familia");
-    var amigos = document.getElementById("Amigos");
-
-    var precio = document.getElementById("precio");
-    var economicos = document.getElementById("Economicos");
-    var turisticos = document.getElementById("Turisticos");
-  
-    var arreglo = [motivo, personas, precio];
-  
-    for (var i = 0; i < arreglo.length; i++) {
-  
-      var elemento = arreglo[i];
-      var texto = elemento.innerHTML.trim();
-  
-      elemento.onclick = function(){
-        alert(texto);
-      };
-  
+        if (precio === null || precio === ""){
+            alert("Por favor responda la pregunta 3");
+            return false;
+        } else {
+            if(precio === 6){
+                arreglo[precio] = 6;
+            } else {
+                arreglo[precio] = 7;
+            }
+        }
     }
-  
+
+    for (var i = 0 ; i < 2 ; i++ ) {
+        console.log(arreglo[i]);
+      }
+
   }
-  
-  asignarEventos();
-  
+
+  asignaValores();
+
+
   
 //funcion que captura datos desde el formulario
 function Destino() {
