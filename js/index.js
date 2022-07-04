22c6a02514ba2545acd8085bad5161ae52429e3d
orne <2115318@ucc.edu.ar>
2022-07-04 17:46:59 -0300

    modifico codigo

--- js/index.js
+++ js/index.js
@@ -1,84 +1,98 @@
 x = 0;
 dx = 2;
 
-document.getElementById("iconoBusqueda").addEventListener("click", mostrar_buscador);
-document.getElementById("contenedorBusqueda").addEventListener("click", ocultar_buscador);
+document.getElementById("iconoBúsqueda").addEventListener("click", mostrar_buscador);
+document.getElementById("contenedorBúsqueda").addEventListener("click", ocultar_buscador);
 
-barraBusqueda =  document.getElementById("barraBusqueda");
-contenedorBusqueda =  document.getElementById("contenedorBusqueda");
-inputBusqueda =       document.getElementById("inputBusqueda");
-cajaBusqueda =        document.getElementById("cajaBusqueda");
+barraBúsqueda = document.getElementById("barraBúsqueda");
+contenedorBúsqueda = document.getElementById("contenedorBúsqueda");
+inputBúsqueda = document.getElementById("inputBúsqueda");
+cajaBúsqueda = document.getElementById("cajaBúsqueda");
 
-function mostrar_buscador(){
+/* Hace visible la barra del buscador de la página de inicio
+* @method mostrar_buscador()
+* @param {string} valor ingresado por el usuario igual a "null"
+* @return none
+*/
 
-    barraBusqueda.style.top = "80px";
-    contenedorBusqueda.style.display = "block";
-    inputBusqueda.focus();
+function mostrar_buscador() {
 
-    if (inputBusqueda.value === ""){
-        cajaBusqueda.style.display = "none";
+    barraBúsqueda.style.top = "80px";
+    contenedorBúsqueda.style.display = "block";
+    inputBúsqueda.focus();
+
+    if (inputBúsqueda.value === "") {
+        cajaBúsqueda.style.display = "none";
     }
 
 }
 
-//Funcion para ocultar el buscador
-function ocultar_buscador(){
+/*Función para ocultar el buscador
+* @method ocultar_buscador ()
+* @return none
+*/
 
-    barraBusqueda.style.top = "-10px";
-    contenedorBusqueda.style.display = "none";
-    inputBusqueda.value = "";
-    cajaBusqueda.style.display = "none";
+function ocultar_buscador() {
+
+    barraBúsqueda.style.top = "-10px";
+    contenedorBúsqueda.style.display = "none";
+    inputBúsqueda.value = "";
+    cajaBúsqueda.style.display = "none";
 
 }
 
+/* Filtrado de búsqueda
+* @method Nombre de la función
+* @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+* @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+* @return Valor que retorna
+*/
 
-//Filtrado de busqueda
+document.getElementById("inputBúsqueda").addEventListener("keyup", buscador_interno);
 
-document.getElementById("inputBusqueda").addEventListener("keyup", buscador_interno);
-
-function buscador_interno(){
-    filter = inputBusqueda.value.toUpperCase();
-    li = cajaBusqueda.getElementsByTagName("li");
+function buscador_interno() {
+    let filter = inputBúsqueda.value.toUpperCase();
+    let li = cajaBúsqueda.getElementsByTagName("li");
 
     //Recorriendo elementos a filtrar mediante los "li"
-    for (i = 0; i < li.length; i++){
+    for (i = 0; i < li.length; i++) {
 
-        a = li[i].getElementsByTagName("a")[0];
-        textValue = a.textContent || a.innerText;
+        let a = li[i].getElementsByTagName("a")[0];
+        let textValue = a.textContent || a.innerText;
 
-        if(textValue.toUpperCase().indexOf(filter) > -1){
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
 
             li[i].style.display = "";
-            cajaBusqueda.style.display = "block";
+            cajaBúsqueda.style.display = "block";
 
-            if (inputBusqueda.value === ""){
-                cajaBusqueda.style.display = "none";
+            if (inputBúsqueda.value === "") {
+                cajaBúsqueda.style.display = "none";
             }
 
-        }else{
+        } else {
             li[i].style.display = "none";
         }
     }
 }
 
 
-function animarAvion() {
-    
+function animarAvión() {
+
     var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext ("2d");
-    
-    var img = new Image ();
+    var ctx = canvas.getContext("2d");
+
+    var img = new Image();
     img.src = "imagenes/avionPrueba.png";
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    img.onload = function(){
+
+    img.onload = function () {
         ctx.drawImage(img, x, 100);
     }
 
-if( x > canvas.width ){
-    x = 0;
-}
+    if (x > canvas.width) {
+        x = 0;
+    }
 
     x += dx;
 }
